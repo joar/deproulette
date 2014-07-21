8f62d9aaf97d44dedd27a265a2fe74c03d153467
Joar Wandborg <joar@wandborg.se>
2014-07-21 23:22:25 +0200

    1.0

--- setup.py
+++ setup.py
@@ -61,28 +61,61 @@ def get_deps():
     for i in range(1, randint(2, 6)):
         deps.append(choice(packages))
 
-    return deps
-
-
-def main(argv=None):
-    deps = get_deps()
 
     _log.info('You got \n- %s\nas dependenc(y|ies). Good luck!',
               '\n- '.join(deps))
 
+    yield deps
+
+
+def main(argv=None):
     setup(
         name='deproulette',
-        version='0.1',
+        version='1.0',
         author='Joar Wandborg',
-        author_email='joar\\x40wandborg.se',
+        author_email='name \\x40 lastname. se',
         description='You never know what you get. Such is life.',
         long_description=open('README.rst').read(),
-        install_requires=deps
+        install_requires=get_deps()
     )
 
 
 if __name__ == '__main__':
     import os
-    logging.basicConfig(level=getattr(logging,
-                                      os.environ.get('LOGLEVEL', 'INFO')))
+
+    _log.setLevel(getattr(logging, os.environ.get('LOGLEVEL', 'INFO')))
+
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(message)s')
+    handler.setFormatter(formatter)
+
+    def decorate_emit(fn):
+    # add methods we need to the class
+        def new(*args):
+            levelno = args[0].levelno
+            if levelno >= logging.CRITICAL:
+                color = '\x1b[31;1m'
+            elif levelno >= logging.ERROR:
+                color = '\x1b[31;1m'
+            elif levelno >= logging.WARNING:
+                color = '\x1b[33;1m'
+            elif levelno >= logging.INFO:
+                color = '\x1b[32;1m'
+            elif levelno >= logging.DEBUG:
+                color = '\x1b[35;1m'
+            else:
+                color = '\x1b[0m'
+
+            # add colored *** in the beginning of the message
+            args[0].msg = "{0}***\x1b[0m {1}".format(color, args[0].msg)
+
+            # new feature i like: bolder each args of message
+            args[0].args = tuple('\x1b[1m' + arg + '\x1b[0m' for arg in args[0].args)
+            return fn(*args)
+        return new
+
+    handler.emit = decorate_emit(handler.emit)
+
+    _log.addHandler(handler)
+
     sys.exit(main(sys.argv))
